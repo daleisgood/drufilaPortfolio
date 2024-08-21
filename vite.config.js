3bc7184386926a690318c0d2e117ddb34474837b
daleisgood <dalerufila@gmail.com>
2024-08-21 11:40:11 +0800

    deploy my portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/drufilaPortfolio",
   plugins: [react()],
   server: {
     port: 4000,
   },
-  base: "/Daleveloper",
+  base: "/drufilaPortfolio",
 });
